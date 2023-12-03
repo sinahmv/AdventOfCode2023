80883d2f4c6c3c880b9fe912ca8af11444365fed
sinah <sinah.mv@gmail.com>
2023-12-03 21:58:16 +0100

    day 2 part 2

--- Day2/Part1/main.go
+++ Day2/Part1/main.go
@@ -14,7 +14,8 @@ func main() {
 	//Input an WhiteSpaces splitten
 	input := string(file)
 	games := SplitLines(input)
-	fmt.Println("Sum of all possible IDs: ", sumUpIDS(part1(games)))
+	fmt.Println("Ergebnis Part 1: ", sumUp(part1(games)))
+	fmt.Println("Ergebnis Part 2: ", sumUp(part2(games)))
 }
 
 func SplitLines(s string) []string {
@@ -39,38 +40,76 @@ func part1(games []string) []int {
 		currentInput := games[i]
 
 		splitInput := strings.Split(currentInput, " ")
-		game,_,_ = strings.Cut(splitInput[1], ":")
-		fmt.Println("Game: ", game)
+		game, _, _ = strings.Cut(splitInput[1], ":")
 
-		gameID,_ = strconv.Atoi(game)
-		fmt.Println("GameID: ", gameID)
+		gameID, _ = strconv.Atoi(game)
 
 		for j := 0; j < len(splitInput); j++ {
 			currentString := splitInput[j]
 
 			if strings.Contains(currentString, "blue") {
-				count, _ := strconv.Atoi(splitInput[j - 1])
+				count, _ := strconv.Atoi(splitInput[j-1])
 				if count > 14 {
 					winGame = false
 				}
 			} else if strings.Contains(currentString, "red") {
-				count, _ := strconv.Atoi(splitInput[j - 1])
+				count, _ := strconv.Atoi(splitInput[j-1])
 				if count > 12 {
 					winGame = false
 				}
 			} else if strings.Contains(currentString, "green") {
-				count, _ := strconv.Atoi(splitInput[j - 1])
+				count, _ := strconv.Atoi(splitInput[j-1])
 				if count > 13 {
 					winGame = false
 				}
-			} 
+			}
 		}
 		if winGame {
 			numbersOfPossibleGames = append(numbersOfPossibleGames, gameID)
 		}
 	}
-	fmt.Println(numbersOfPossibleGames)
 	return numbersOfPossibleGames
+}
+
+func part2(games []string) []int {
+
+	powers := make([]int, 0)
+	multiplyCounts := 0
+
+	for i := 0; i < len(games); i++ {
+
+		currentLowestRed := -1
+		currentLowestBlue := -1
+		currentLowestGreen := -1
+
+		currentInput := games[i]
+
+		splitInput := strings.Split(currentInput, " ")
+
+		for j := 0; j < len(splitInput); j++ {
+			currentString := splitInput[j]
+
+			if strings.Contains(currentString, "blue") {
+				count, _ := strconv.Atoi(splitInput[j-1])
+				if count > currentLowestBlue {
+					currentLowestBlue = count
+				}
+			} else if strings.Contains(currentString, "red") {
+				count, _ := strconv.Atoi(splitInput[j-1])
+				if count > currentLowestRed {
+					currentLowestRed = count
+				}
+			} else if strings.Contains(currentString, "green") {
+				count, _ := strconv.Atoi(splitInput[j-1])
+				if count > currentLowestGreen {
+					currentLowestGreen = count
+				}
+			}
+		}
+		multiplyCounts = currentLowestBlue * currentLowestGreen * currentLowestRed
+		powers = append(powers, multiplyCounts)
+	}
+	return powers
 }
 
 func checkIfGameIsPossible(countRed int, countBlue int, countGreen int) bool {
@@ -81,7 +120,7 @@ func checkIfGameIsPossible(countRed int, countBlue int, countGreen int) bool {
 	return gameIsPossible
 }
 
-func sumUpIDS(numbersOfPossibleGames []int) int {
+func sumUp(numbersOfPossibleGames []int) int {
 	sum := 0
 	for i := 0; i < len(numbersOfPossibleGames); i++ {
 		sum = sum + numbersOfPossibleGames[i]
